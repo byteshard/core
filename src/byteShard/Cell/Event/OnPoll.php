8d3265c9905010475c0ea2938931745a399afbe8
Lars Hennig <lars@bespin-studios.com>
2024-03-15 13:48:28 +0100

    feat: add implicit events That way if for example the interface OnPoll is implemented, you don't have to explicitly add the OnPoll event to the cell. This should be implemented for all cell based events.

--- src/byteShard/Cell/Event/OnPoll.php
+++ src/byteShard/Cell/Event/OnPoll.php
@@ -7,31 +7,21 @@
 namespace byteShard\Cell\Event;
 
 use byteShard\Action\Cell\ContinuePolling;
-use byteShard\Enum\AccessType;
+use byteShard\Event\OnPollInterface;
 use byteShard\Internal\Event\Event;
 use byteShard\Internal\Event\EventMigrationInterface;
-use byteShard\Session;
+use byteShard\Internal\Event\ImplicitEventInterface;
 
-class OnPoll extends Event implements EventMigrationInterface
+class OnPoll extends Event implements EventMigrationInterface, ImplicitEventInterface
 {
     protected static string $event = 'onPoll';
+
+    public function getImplicitInterfaceClass(): string {
+        return OnPollInterface::class;
+    }
 
     public function getClientArray(string $cellNonce): array
     {
         return ['onPoll' => ContinuePolling::getPollId($cellNonce)];
-        $time = microtime();
-        $encrypted = [
-            'i' => $time,
-            'a' => AccessType::RW,
-            't' => 'poll'
-        ];
-
-        // the nonce should be unique per object, but we need to be able to recreate it for object access in actions.
-        // The solution is to take a part of the stored nonce and add the object name, generate a md5 from this and use the first 24 characters
-        // The nonce will be unique per client rendering as the cell nonce is recycled whenever content is reloaded.
-        // That way we can manipulate objects of an existing client form, but we're also in compliance with security recommendations
-        $nonce = substr(md5($cellNonce.$time), 0, 24);
-        return ['onPoll' => Session::encrypt(json_encode($encrypted), $nonce)];
-        //return ['onPoll' => Session::encrypt(json_encode(['i' => 'foo']), $cellNonce)];
     }
 }
